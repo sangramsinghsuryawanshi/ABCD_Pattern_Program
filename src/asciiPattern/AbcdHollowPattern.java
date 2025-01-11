66d27a856a2c1c9096ca52d72be2208360debb45
Suryawanshi Sangramsingh Sambhaji <suryawanshisangramsinh@gmail.com>
2025-01-11 22:15:32 +0530

    updated

--- src/asciiPattern/AbcdHollowPattern.java
+++ src/asciiPattern/AbcdHollowPattern.java
@@ -35,7 +35,6 @@ public class AbcdHollowPattern
 	}
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter n number: ");
 		int n = sc.nextInt();
